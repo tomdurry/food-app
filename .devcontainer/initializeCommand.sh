d9b59539067ae471627a1a71a380ba7777e0709f
mori_yuk <tomdurry1006@gmail.com>
2024-05-17 21:31:32 +0000

    Do not display git user identity confirmation in devcontainer  #51

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -4,3 +4,9 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     ssh-add --apple-use-keychain ~/.ssh/id_rsa
 fi
+
+# create .env
+cat <<-EOF > .devcontainer/.env
+GIT_NAME=$(git config --get user.name)
+GIT_EMAIL=$(git config --get user.email)
+EOF
