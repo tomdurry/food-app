80a9d9581c2efb152b1e057994a94920aab27c2e
mori_yuk <tomdurry1006@gmail.com>
2024-07-05 20:15:24 +0000

    Revert "Merge pull request #68 from tomdurry/splitting_components_#67"
    This reverts commit c2e02792e95cd267ec8e16aaa2b5e963cff54fad, reversing
    changes made to 288fd54b633a5804849e57fca18275a711a22e8c.

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,15 +1,7 @@
-export type CsrfToken = {
-  csrf_token: string;
-};
-
-export type Credential = {
-  email: string;
-  password: string;
-};
-
-export type Recipe = {
-  recipe_name: string;
-  ingredients: { ingredient: string; quantity: string }[];
-  instructions: string | string[];
-  image_url: string;
-}
+  export type CsrfToken = {
+    csrf_token: string
+  }
+  export type Credential = {
+    email: string
+    password: string
+  }
