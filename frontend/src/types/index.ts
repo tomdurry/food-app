291370a06fdc2e0972687e5cd11090b44ebcbdfc
mori_yuk <tomdurry1006@gmail.com>
2024-07-06 18:48:48 +0000

    Refactoring the recipe generation screen #72

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,7 +1,15 @@
-  export type CsrfToken = {
-    csrf_token: string
-  }
-  export type Credential = {
-    email: string
-    password: string
-  }
+export type Recipe = {
+  recipe_name: string;
+  ingredients: { ingredient: string; quantity: string }[];
+  instructions: string | string[];
+  image_url: string;
+}
+
+export type CsrfToken = {
+  csrf_token: string
+}
+
+export type Credential = {
+  email: string
+  password: string
+}
