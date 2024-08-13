6941228cf59f812349ce4cb068898357ab965b98
mori_yuk <tomdurry1006@gmail.com>
2024-08-13 10:33:10 +0000

    Setting up the CDK for Terraform #94

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,3 +8,9 @@ git config --global user.email "${GIT_EMAIL}"
 git config --global push.autoSetupRemote true
 
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+# Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update && sudo apt-get install -y terraform
