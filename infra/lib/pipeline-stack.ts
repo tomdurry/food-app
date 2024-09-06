3a117c123bd5f51cf830e7467847423ceffd7f1b
mori_yuk <tomdurry1006@gmail.com>
2024-09-06 23:32:55 +0000

    Fixing buildspec.yml and pipeline #100

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -7,7 +7,6 @@ import { IamRolePolicy } from "@cdktf/provider-aws/lib/iam-role-policy";
 import { Codepipeline } from "@cdktf/provider-aws/lib/codepipeline";
 import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket";
 import { CodestarconnectionsConnection } from "@cdktf/provider-aws/lib/codestarconnections-connection";
-import * as path from "path";
 
 export class PipelineStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -64,7 +63,7 @@ export class PipelineStack extends TerraformStack {
       serviceRole: codeBuildRole.arn,
       source: {
         type: "CODEPIPELINE",
-        buildspec: path.join(__dirname, "../buildspec/buildspec.yml"),
+        buildspec: "infra/lib/buildspec/buildspec.yml",
       },
       environment: {
         computeType: "BUILD_GENERAL1_SMALL",
