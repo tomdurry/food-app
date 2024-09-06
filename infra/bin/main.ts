061ac24bacb3cf945b58b2753dfb7412d4366e3a
mori_yuk <tomdurry1006@gmail.com>
2024-09-06 22:02:10 +0000

    Building and testing pipelines #100

--- infra/bin/main.ts
+++ infra/bin/main.ts
@@ -1,8 +1,10 @@
 import { App } from "cdktf";
 import { AccountStack } from "../lib/account-stack";
+import { PipelineStack } from "../lib/pipeline-stack";
 import { RecipeGenerationStack } from "../lib/recipe-generation-stack";
 
 const app = new App();
 new AccountStack(app, "account-stack");
+new PipelineStack(app, "pipeline-stack");
 new RecipeGenerationStack(app, "recipe-generation-stack");
 app.synth();
