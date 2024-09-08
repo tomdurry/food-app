1c06e3dfbf7130fa895ce2364138b8bf0c67b726
mori_yuk <tomdurry1006@gmail.com>
2024-09-08 12:03:09 +0000

    Adding watcher roles and trust relationships  #101

--- infra/lib/account-stack.ts
+++ infra/lib/account-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from "constructs";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { IamUser } from "@cdktf/provider-aws/lib/iam-user";
 import { IamRole } from "@cdktf/provider-aws/lib/iam-role";
+import { IamPolicy } from "@cdktf/provider-aws/lib/iam-policy";
 import { IamPolicyAttachment } from "@cdktf/provider-aws/lib/iam-policy-attachment";
 
 export class AccountStack extends TerraformStack {
@@ -13,11 +14,11 @@ export class AccountStack extends TerraformStack {
       region: "ap-northeast-1",
     });
 
-    const user = new IamUser(this, "AdministratorUser", {
-      name: "AdministratorUser",
+    const adminUser = new IamUser(this, "AdministratorUser", {
+      name: "Administrator",
     });
 
-    const role = new IamRole(this, "AdministratorRole", {
+    const adminRole = new IamRole(this, "AdministratorRole", {
       name: "AdministratorRole",
       assumeRolePolicy: JSON.stringify({
         Version: "2012-10-17",
@@ -26,23 +27,83 @@ export class AccountStack extends TerraformStack {
             Action: "sts:AssumeRole",
             Effect: "Allow",
             Principal: {
-              Service: "ec2.amazonaws.com",
+              AWS: adminUser.arn,
             },
           },
         ],
       }),
     });
 
-    new IamPolicyAttachment(this, "userPolicyAttachment", {
-      name: "userPolicyAttachment",
-      users: [user.name],
+    const adminAssumeRolePolicy = new IamPolicy(this, "adminAssumeRolePolicy", {
+      name: "AdminAssumeRolePolicy",
+      policy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Resource: adminRole.arn,
+          },
+        ],
+      }),
+    });
+
+    new IamPolicyAttachment(this, "adminUserAssumeRolePolicyAttachment", {
+      name: "AdminUserAssumeRolePolicyAttachment",
+      users: [adminUser.name],
+      policyArn: adminAssumeRolePolicy.arn,
+    });
+
+    new IamPolicyAttachment(this, "adminRolePolicyAttachment", {
+      name: "AdminRolePolicyAttachment",
+      roles: [adminRole.name],
       policyArn: "arn:aws:iam::aws:policy/AdministratorAccess",
     });
 
-    new IamPolicyAttachment(this, "rolePolicyAttachment", {
-      name: "rolePolicyAttachment",
-      roles: [role.name],
-      policyArn: "arn:aws:iam::aws:policy/AdministratorAccess",
+    const watcher = new IamUser(this, "watcher", {
+      name: "watcher",
+    });
+
+    const watcherRole = new IamRole(this, "WatcherRole", {
+      name: "WatcherRole",
+      assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Principal: {
+              AWS: watcher.arn,
+            },
+          },
+        ],
+      }),
+    });
+
+    const watchAssumeRolePolicy = new IamPolicy(this, "watchAssumeRolePolicy", {
+      name: "WatchAssumeRolePolicy",
+      policy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Resource: watcherRole.arn,
+          },
+        ],
+      }),
+    });
+
+    new IamPolicyAttachment(this, "watchUserAssumeRolePolicyAttachment", {
+      name: "WatchUserAssumeRolePolicyAttachment",
+      users: [watcher.name],
+      policyArn: watchAssumeRolePolicy.arn,
+    });
+
+    new IamPolicyAttachment(this, "watchRolePolicyAttachment", {
+      name: "WatchRolePolicyAttachment",
+      roles: [watcherRole.name],
+      policyArn: "arn:aws:iam::aws:policy/ReadOnlyAccess",
     });
   }
 }
