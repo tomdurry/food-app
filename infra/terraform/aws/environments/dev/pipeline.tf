ff4dc063a14498d37fd1708e61ae08dfc91aa80a
mori_yuk <tomdurry1006@gmail.com>
2024-09-16 00:33:54 +0000

    test

--- infra/terraform/aws/environments/dev/pipeline.tf
+++ infra/terraform/aws/environments/dev/pipeline.tf
@@ -20,13 +20,11 @@ resource "aws_iam_role" "codebuild_service_role" {
       }
     ]
   })
-}
 
-resource "aws_iam_role_policy_attachment" "codebuild_service_role_admin" {
-  role       = aws_iam_role.codebuild_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess"
+  ]
 }
-
 resource "aws_codebuild_project" "food_app_deployer" {
   name         = "food-app-deployer"
   service_role = aws_iam_role.codebuild_service_role.arn
