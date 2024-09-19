729d8e9cef7a3a4e8615b5792eb1bc1a38d31c13
mori_yuk <tomdurry1006@gmail.com>
2024-09-19 00:45:58 +0000

    Webhook branch name changed to main #107

--- infra/terraform/aws/environments/dev/pipeline.tf
+++ infra/terraform/aws/environments/dev/pipeline.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_admin_policy" {
 
 resource "aws_codebuild_project" "food_app_deployer" {
   name         = "food-app-deployer"
-  service_role = aws_iam_role.codebuild_role.arn  # local変数を直接参照に変更
+  service_role = aws_iam_role.codebuild_role.arn # local変数を直接参照に変更
 
   source {
     type      = "CODEPIPELINE"
@@ -80,7 +80,7 @@ resource "aws_codepipeline" "food_app_pipeline" {
       configuration = {
         ConnectionArn    = aws_codestarconnections_connection.codestar_connection.arn
         FullRepositoryId = "tomdurry/food-app"
-        BranchName       = "buildspec-test"
+        BranchName       = "main"
       }
     }
   }
