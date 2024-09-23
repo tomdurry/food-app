4b65bdc3a7aefdb0d3b0cd3f035dda2dc11adce3
mori_yuk <tomdurry1006@gmail.com>
2024-09-23 10:00:01 +0000

    Modularization  #117

--- infra/terraform/aws/environments/dev/main.tf
+++ infra/terraform/aws/environments/dev/main.tf
@@ -1,1 +1,13 @@
+module "account" {
+  source = "../../modules/account"
+}
 
+module "pipeline" {
+  source = "../../modules/pipeline"
+}
+
+module "recipeGeneration" {
+  source      = "../../modules/recipeGeneration"
+  environment = "dev"
+  project     = "food-app"
+}
