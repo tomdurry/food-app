f24044d052204ee6d567dec5ea696e27c53d8e02
mori_yuk <tomdurry1006@gmail.com>
2024-11-04 22:31:41 +0000

    Fix eks policy  #141

--- infra/terraform/aws/application/modules/container/eks-policy/main.tf
+++ infra/terraform/aws/application/modules/container/eks-policy/main.tf
@@ -16,7 +16,7 @@ resource "aws_eks_access_policy_association" "eks-admin-policy" {
 
 resource "aws_eks_access_policy_association" "eks-cluster-admin-policy" {
   cluster_name  = "food-app-cluster-prod"
-  policy_arn    = "arn:aws:iam::aws:policy/AmazonEKSClusterAdminPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = data.aws_iam_role.admin_role.arn
 
   access_scope {
