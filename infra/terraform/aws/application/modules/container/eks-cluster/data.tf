50406ead4f4d393ca8ffc49b26de0e9ae4f7f634
mori_yuk <tomdurry1006@gmail.com>
2024-11-10 16:24:24 +0000

    test #141

---
+++ infra/terraform/aws/application/modules/container/eks-cluster/data.tf
@@ -0,0 +1,9 @@
+data "aws_iam_policy_document" "node_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
