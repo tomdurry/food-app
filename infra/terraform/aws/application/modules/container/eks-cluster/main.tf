b64e41550cc5f734fff4942b2a8ce83ea2c32436
mori_yuk <tomdurry1006@gmail.com>
2024-11-11 15:10:12 +0000

    Fix policy #141

--- infra/terraform/aws/application/modules/container/eks-cluster/main.tf
+++ infra/terraform/aws/application/modules/container/eks-cluster/main.tf
@@ -26,6 +26,11 @@ resource "aws_iam_role_policy_attachment" "eks_cni" {
   role       = aws_iam_role.node_group_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group_role.name
+}
+
 
 resource "aws_eks_node_group" "this_node_group" {
   cluster_name    = aws_eks_cluster.this.name
