d9d83d7d36c7b28b495d7af6ac1712c8f058a205
mori_yuk <tomdurry1006@gmail.com>
2024-11-12 16:39:45 +0000

    test #141

--- infra/terraform/aws/application/modules/rds/main.tf
+++ infra/terraform/aws/application/modules/rds/main.tf
@@ -1,11 +1,29 @@
+# resource "aws_security_group" "rds_sg" {
+#   name   = "rds-sg"
+#   vpc_id = var.vpc_id
+
+#   ingress {
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
 resource "aws_security_group" "rds_sg" {
   name   = "rds-sg"
   vpc_id = var.vpc_id
-
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -16,6 +34,7 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 
 resource "aws_db_instance" "postgres" {
   allocated_storage      = 20
