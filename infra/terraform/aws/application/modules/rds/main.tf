c0fb844f45a9ad832e7b86c0449c09b13daa55bf
mori_yuk <tomdurry1006@gmail.com>
2024-11-13 07:50:43 +0000

    test #141

--- infra/terraform/aws/application/modules/rds/main.tf
+++ infra/terraform/aws/application/modules/rds/main.tf
@@ -1,30 +1,11 @@
-# resource "aws_security_group" "rds_sg" {
-#   name   = "rds-sg"
-#   vpc_id = var.vpc_id
-
-#   ingress {
-#     from_port   = 5432
-#     to_port     = 5432
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
 resource "aws_security_group" "rds_sg" {
   name   = "rds-sg"
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -35,6 +16,25 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# resource "aws_security_group" "rds_sg" {
+#   name   = "rds-sg"
+#   vpc_id = var.vpc_id
+
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
 resource "aws_db_instance" "postgres" {
