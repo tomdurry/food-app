0da8b1aadf50d75d5c9c9a4409fceecbe9e761e7
mori_yuk <tomdurry1006@gmail.com>
2024-11-18 05:54:51 +0000

    Fix Dockerfile  #141

--- infra/terraform/aws/application/modules/lambda/database-creation/src/create_database.py
+++ infra/terraform/aws/application/modules/lambda/database-creation/src/create_database.py
@@ -2,21 +2,17 @@ import boto3
 import psycopg2
 import os
 
-# SSM クライアントの作成
 ssm = boto3.client('ssm')
 
 def lambda_handler(event, context):
-    # パラメータストアから RDS のエンドポイントを取得
     rds_endpoint = ssm.get_parameter(Name='/prod/rds_endpoint', WithDecryption=True)['Parameter']['Value']
     
-    # 接続情報の設定
     db_name = "postgres"
     db_user = "yukihiro"
     db_password = "Yuki3769"
     db_port = 5432
     
     try:
-        # RDS に接続
         conn = psycopg2.connect(
             host=rds_endpoint,
             database=db_name,
@@ -26,7 +22,6 @@ def lambda_handler(event, context):
         )
         conn.autocommit = True
         
-        # カーソルを作成してデータベースを作成
         cursor = conn.cursor()
         cursor.execute("CREATE DATABASE yukihiro;")
         cursor.close()
