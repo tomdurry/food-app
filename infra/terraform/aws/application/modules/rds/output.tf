028e53b9421e62a6f519b54ed71b218ebc32b27f
mori_yuk <tomdurry1006@gmail.com>
2024-11-23 06:04:20 +0000

    Delete output of eks_sg  #141

--- infra/terraform/aws/application/modules/rds/output.tf
+++ infra/terraform/aws/application/modules/rds/output.tf
@@ -2,8 +2,3 @@ output "lambda_sg_id" {
   value       = aws_security_group.lambda_sg.id
   description = "The ID of the Lambda security group"
 }
-
-output "eks_sg_id" {
-  value       = aws_security_group.eks_sg.id
-  description = "The ID of the Lambda security group"
-}
