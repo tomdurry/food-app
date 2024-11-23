7151278c23b4df29fa12d52b3ca6761d320fbd8b
mori_yuk <tomdurry1006@gmail.com>
2024-11-23 02:30:59 +0000

    test #141

--- infra/terraform/aws/application/modules/rds/main.tf
+++ infra/terraform/aws/application/modules/rds/main.tf
@@ -15,10 +15,14 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda_sg.id]
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # security_groups = [
+    #   aws_security_group.lambda_sg.id,
+    #   aws_security_group.eks_sg.id
+    # ]
   }
 
   egress {
