c3d0b004e001d4cddaeee0228797719b4253675a
mori_yuk <tomdurry1006@gmail.com>
2024-12-28 06:53:04 +0000

    test #143

--- infra/terraform/aws/application/modules/rds/main.tf
+++ infra/terraform/aws/application/modules/rds/main.tf
@@ -62,7 +62,7 @@ resource "aws_db_instance" "postgres" {
   password               = "Yuki3769"
   parameter_group_name   = "default.postgres16"
   skip_final_snapshot    = true
-  publicly_accessible    = false
+  publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet.id
 }
