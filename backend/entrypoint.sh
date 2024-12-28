15717360e58e3f0acf9ee5e8135b3ec8c173f72c
mori_yuk <tomdurry1006@gmail.com>
2024-12-28 03:40:55 +0000

    migration test #143

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations..."
 /app/main migrate
-
-echo "Starting application..."
 /app/main
--- backend/migrate/migrate.go
+++ backend/migrate/migrate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/tomdurry/food-app/db"
 	"github.com/tomdurry/food-app/model"
@@ -11,6 +12,9 @@ func main() {
 	dbConn := db.NewDB()
 	defer db.CloseDB(dbConn)
 	fmt.Println("Running migrations...")
-	dbConn.AutoMigrate(&model.User{}, &model.Recipe{})
+	err := dbConn.AutoMigrate(&model.User{}, &model.Recipe{})
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
 	fmt.Println("Successfully Migrated")
 }
