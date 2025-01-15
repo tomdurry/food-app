2bb5551916b6883c276eb7876806ca4e30d3144b
mori_yuk <tomdurry1006@gmail.com>
2025-01-15 11:19:19 +0000

    Top page changes #151

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import useStore from '../store'
 import { useMutateAuth } from '../hooks/useMutateAuth'
 
@@ -9,51 +9,65 @@ export const NavigationBar = () => {
     setIsLoginForm: state.setIsLoginForm,
   }))
 
+  const navigate = useNavigate()
+
   const logout = async () => {
     await logoutMutation.mutateAsync()
+  }
+
+  const handleAuthRedirect = (isLoginForm: boolean) => {
+    setIsLoginForm(isLoginForm)
+    navigate('/auth')
+  }
+
+  const handleFavoriteClick = () => {
+    if (!isLogin) {
+      setIsLoginForm(true)
+      navigate('/auth')
+    } else {
+      navigate('/favorite')
+    }
   }
 
   return (
     <div className="fixed top-0 left-0 w-full bg-orange-500 shadow-md z-10">
       <div className="max-w-7xl mx-auto px-4 py-2 flex justify-between items-center">
         <div className="flex items-center">
-          <Link
-            to={isLogin ? '/generate' : '/'}
-            className="text-3xl font-extrabold"
-          >
+          <Link to="/generate" className="text-3xl font-extrabold">
             みんなの食卓
           </Link>
         </div>
         <div className="flex">
-          {isLogin && (
-            <>
-              <Link to="/generate" className="mx-2 text-lg">
-                レシピ生成
-              </Link>
-              <Link to="/favorite" className="mx-2 text-lg">
-                お気に入りレシピ
-              </Link>
-              <button onClick={logout} className="mx-2 text-lg">
-                ログアウト
-              </button>
-            </>
-          )}
-          {!isLogin && (
-            <>
-              <button
-                onClick={() => setIsLoginForm(true)}
-                className="mx-2 text-lg"
-              >
-                ログイン
-              </button>
-              <button
-                onClick={() => setIsLoginForm(false)}
-                className="mx-2 text-lg"
-              >
-                ユーザー登録
-              </button>
-            </>
-          )}
+          <>
+            <Link to="/generate" className="mx-2 text-lg">
+              レシピ生成
+            </Link>
+            <button onClick={handleFavoriteClick} className="mx-2 text-lg">
+              お気に入りレシピ
+            </button>
+            {isLogin ? (
+              <>
+                <button onClick={logout} className="mx-2 text-lg">
+                  ログアウト
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={() => handleAuthRedirect(true)}
+                  className="mx-2 text-lg"
+                >
+                  ログイン
+                </button>
+                <button
+                  onClick={() => handleAuthRedirect(false)}
+                  className="mx-2 text-lg"
+                >
+                  ユーザー登録
+                </button>
+              </>
+            )}
+          </>
         </div>
       </div>
     </div>
