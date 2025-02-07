524abf1dfa28c8258bda846d587e6827bbd8e16a
mori_yuk <tomdurry1006@gmail.com>
2025-02-07 05:18:39 +0000

    Setup of vitest #161

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,5 +1,3 @@
 test('always passes', () => {
   expect(true).toBe(true)
 })
-
-export {}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
-/// <reference types="vite/client" />
-
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./vitest-setup.ts'],
+  },
   server: {
     port: 3000,
     hmr: {
