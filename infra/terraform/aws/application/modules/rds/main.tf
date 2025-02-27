a56cf0884d66089f6d496b7b0bd9ea8108578e57
mori_yuk <tomdurry1006@gmail.com>
2025-02-27 07:36:04 +0000

    test #180

--- infra/terraform/aws/application/modules/rds/main.tf
+++ infra/terraform/aws/application/modules/rds/main.tf
@@ -66,7 +66,7 @@ resource "aws_db_instance" "postgres" {
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet.id
 
-  multi_az = true
+  # multi_az = true
 
   tags = {
     Project     = var.project
